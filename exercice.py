f816feb30b5e5e6acb3f11033ff22642818b0558
ninalounici <nina.lounici@polymtl.ca>
2020-09-14 23:34:30 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return math.radians(angle_degs + angle_mins + angle_secs)
+    return math.radians(angle_degs + angle_mins + angle_secs/)
 
 
 def to_degrees(angle_rads: float) -> tuple:
